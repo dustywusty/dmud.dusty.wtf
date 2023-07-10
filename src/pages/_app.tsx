d2bf8f23542a3a1aa2a67a58751a40017cba92f7
dusty <dustyc@me.com>
2023-07-10 00:45:10 -0600

    wrap w/ our provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,14 @@ import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/globals.css";
 
+import PlayerProvider from "../providers/PlayerProvider";
+
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <main>
-      <Component {...pageProps} />
+      <PlayerProvider>
+        <Component {...pageProps} />
+      </PlayerProvider>
       <Analytics />
       <a href="https://github.com/dustywusty/dmud.dusty.wtf" target="_blank" className="pi-icon">&#960;</a>
     </main>
